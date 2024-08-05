b3037bc29353babc5121f881772e3bc9223a72c7
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-08-05 15:41:50 +0530

    (upkeep) add tests for Node class, add rubocop

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,14 @@
+# class Node implements a node as a basic unit of Tree class
+# class variables:
+#  @value
+#  @left
+#  @right
+# class methods:
+#  initialize
 class Node
   attr_accessor :left, :right, :value
 
-  def initialize(val=nil, left=nil, right=nil)
+  def initialize(val = nil, left = nil, right = nil)
     @value = val
     @left = left
     @right = right
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,10 +1,74 @@
-require './lib/main.rb'
+require "./lib/main"
 
-describe Node do
-  describe "#initialize" do
+describe Node do # rubocop:disable Metrics/BlockLength
+  context "#initialize with nil" do
+    node = Node.new
+
     it "creates a new node with nil value" do
-      node = Node.new
       expect(node.value).to eql(nil)
+    end
+
+    it "creates a new node with nil left" do
+      expect(node.left).to eql(nil)
+    end
+
+    it "creates a new node with nil right" do
+      expect(node.right).to eql(nil)
+    end
+  end
+
+  context "#initialize only with value" do
+    node = Node.new("test")
+
+    it "creates a new node with given value" do
+      expect(node.value).to eql("test")
+    end
+
+    it "created a new node with left nil" do
+      expect(node.left).to eql(nil)
+    end
+
+    it "creates a new node with right nil" do
+      expect(node.right).to eql(nil)
+    end
+  end
+
+  context "#initialize with value and left node" do
+    node1 = Node.new("child")
+    node2 = Node.new("parent", node1)
+
+    it "creates a new node with given value" do
+      expect(node2.value).to eql("parent")
+    end
+
+    it "creates a new node with a left node" do
+      expect(node2.left).to be(node1)
+    end
+
+    it "retains the value of the left node" do
+      expect(node2.left.value).to eql("child")
+    end
+  end
+
+  context "#initialize with value and both nodes" do
+    node_l = Node.new("left")
+    node_r = Node.new("right")
+    node   = Node.new("parent", node_l, node_r)
+
+    it "creates a new node with a left node" do
+      expect(node.left).to be(node_l)
+    end
+
+    it "created a new node with a right node" do
+      expect(node.right).to be(node_r)
+    end
+
+    it "retains the value of the left node" do
+      expect(node.left.value).to eql("left")
+    end
+
+    it "retians the value of the right node" do
+      expect(node.right.value).to eql("right")
     end
   end
 end
